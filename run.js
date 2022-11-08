7570e8d35a5d646f15a960473363c8af1588b883
Roland Tahker <rolandtahker@gmail.com>
2022-11-08 10:56:12 +0200

    08.11.22

--- run.js
+++ run.js
@@ -5,6 +5,12 @@ import chalk from 'chalk';
 
 write(chalk.blue.bgGreen('Hello chalk'));
 
+for(let i=0; i<16; i++){
+    for(let j=0; j<16; j++){
+        write(chalk.ansi256(color)(color.padEnd(4,' ')));
+    }
+    write('\n');
+}
 
 
 
